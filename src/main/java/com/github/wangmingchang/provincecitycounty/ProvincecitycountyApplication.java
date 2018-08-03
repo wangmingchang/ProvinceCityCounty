9aceb52ceec6e9c0ec37868b7c49c17e84cdf9ce
王明昌 <785609735@qq.com>
2018-08-03 15:19:18 +0800

    初化项目

---
+++ src/main/java/com/github/wangmingchang/provincecitycounty/ProvincecitycountyApplication.java
@@ -0,0 +1,12 @@
+package com.github.wangmingchang.provincecitycounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvincecitycountyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvincecitycountyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/wangmingchang/provincecitycounty/ProvincecitycountyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.wangmingchang.provincecitycounty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProvincecitycountyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
